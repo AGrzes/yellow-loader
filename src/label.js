4d4c4da5645ddd82016cce29c00f85248bf53f69
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-18 18:00:34 +0100

    Label handling

---
+++ src/label.js
@@ -0,0 +1,27 @@
+
+const _  = require('lodash')
+function labelFromKey(key){
+  return _.startCase(key)
+}
+
+function labelFromEntity(entity,keyFunction){
+  if (entity.$label){
+    return entity.$label
+  } else if (entity.label){
+    return entity.label
+  } else if (keyFunction) {
+    const key = keyFunction(entity)
+    if (key){
+      return labelFromKey(key)
+    }
+  }
+}
+
+function set(source,keyFunction){
+  return source.map((entity)=>{
+    entity.$label = labelFromEntity(entity,keyFunction)
+    return entity
+  })
+}
+
+module.exports.set = set
