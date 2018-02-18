23bbb6da99190be4e3839ffe75c629a70eafac01
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-18 15:02:32 +0100

    Handling multiple documents

--- src/json.js
+++ src/json.js
@@ -1,12 +1,22 @@
 const rx = require('rxjs')
 const fs = require('fs')
+const _ = require('lodash')
 const readFile = rx.Observable.bindNodeCallback(fs.readFile)
 
 function load(source) {
   return source.filter((entry) => /.*\.json/.test(entry.fileName)).mergeMap((entry) =>
-    readFile(entry.path).map((contents) => Object.assign(JSON.parse(contents), {
-      $metadata: entry
-    }))
+    readFile(entry.path).mergeMap((contents) => {
+      const json = JSON.parse(contents)
+      if (_.isArray(json)) {
+        return rx.Observable.of(..._.map(json, (document) => Object.assign(document, {
+          $metadata: entry
+        })))
+      } else {
+        return rx.Observable.of(Object.assign(json, {
+          $metadata: entry
+        }))
+      }
+    })
   )
 }
 
--- test/json.js
+++ test/json.js
@@ -6,11 +6,12 @@ describe('json', () => {
   before(() => {
     mock({
       '/file1.json': '{"key":"value"}',
+      '/file2.json': '[{"key1":"value1"},{"key2":"value2"}]',
     })
   })
   after(() => mock.restore())
 
-  it('Should load json scans', function (done) {
+  it('Should load json', function (done) {
     json.load(rxjs.Observable.of({
       path: '/file1.json',
       fileName: 'file1.json'
@@ -21,6 +22,29 @@ describe('json', () => {
           fileName: 'file1.json'
         },
         "key": "value"
+      }])
+      done()
+    }, (error) => done(error))
+  })
+
+  it('Should load multiple documents', function (done) {
+    json.load(rxjs.Observable.of({
+      path: '/file2.json',
+      fileName: 'file2.json'
+    })).toArray().subscribe((entries) => {
+      expect(entries).to.containSubset([{
+        $metadata: {
+          path: '/file2.json',
+          fileName: 'file2.json'
+        },
+        "key1": "value1"
+      }])
+      expect(entries).to.containSubset([{
+        $metadata: {
+          path: '/file2.json',
+          fileName: 'file2.json'
+        },
+        "key2": "value2"
       }])
       done()
     }, (error) => done(error))
