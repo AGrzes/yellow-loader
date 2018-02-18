e08acade6ebbf8a323de625a2d40e5e6333e5952
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-18 18:02:19 +0100

    Test for label transformation

--- test/label.js
+++ test/label.js
@@ -48,4 +48,16 @@ describe('label', () => {
       done()
     }, (error) => done(error))
   })
+
+  it('Should transform $key', function (done) {
+    label.set(rxjs.Observable.of({
+      key: "label-label"
+    }),(entry)=>entry.key).toArray().subscribe((entries) => {
+      expect(entries).to.have.property('length', 1)
+      expect(entries).to.containSubset([{
+        $label: "Label Label"
+      }])
+      done()
+    }, (error) => done(error))
+  })
 })
