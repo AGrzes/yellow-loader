d5730f3d8a9e647a266ce4e58f4f0273bfe7785c
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-18 17:47:41 +0100

    Using key as

--- src/key.js
+++ src/key.js
@@ -1,6 +1,9 @@
 
 function set(source){
-  return source
+  return source.map((entry)=>{
+    entry.$key = entry.$key || entry.key
+    return entry
+  })
 }
 
 module.exports.set = set
--- test/key.js
+++ test/key.js
@@ -2,7 +2,7 @@ const expect = require('chai').use(require('chai-subset')).expect
 const key = require('../src/key')
 const rxjs = require('rxjs')
 describe('json', () => {
-  it('Should keep key', function (done) {
+  it('Should keep $key', function (done) {
     key.set(rxjs.Observable.of({
       $key: "key"
     })).toArray().subscribe((entries) => {
@@ -13,5 +13,15 @@ describe('json', () => {
       done()
     }, (error) => done(error))
   })
-
+  it('Should use key', function (done) {
+    key.set(rxjs.Observable.of({
+      key: "key"
+    })).toArray().subscribe((entries) => {
+      expect(entries).to.have.property('length', 1)
+      expect(entries).to.containSubset([{
+        $key: "key"
+      }])
+      done()
+    }, (error) => done(error))
+  })
 })
