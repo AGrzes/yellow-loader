0ce2631132b742bbedcde4ad71e5ed871940f453
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-18 17:53:29 +0100

    Handling label as a key

--- src/key.js
+++ src/key.js
@@ -1,8 +1,26 @@
 
-function set(source){
-  return source.map((entry)=>{
-    entry.$key = entry.$key || entry.key
-    return entry
+const _  = require('lodash')
+function keyFromLabel(label){
+  return _.kebabCase(label)
+}
+
+function keyFromEntity(entity,labelFunction){
+  if (entity.$key){
+    return entity.$key
+  } else if (entity.key){
+    return entity.key
+  } else if (labelFunction) {
+    const label = labelFunction(entity)
+    if (label){
+      return keyFromLabel(label)
+    }
+  }
+}
+
+function set(source,labelFunction){
+  return source.map((entity)=>{
+    entity.$key = keyFromEntity(entity,labelFunction)
+    return entity
   })
 }
 
--- test/key.js
+++ test/key.js
@@ -1,7 +1,7 @@
 const expect = require('chai').use(require('chai-subset')).expect
 const key = require('../src/key')
 const rxjs = require('rxjs')
-describe('json', () => {
+describe('key', () => {
   it('Should keep $key', function (done) {
     key.set(rxjs.Observable.of({
       $key: "key"
@@ -17,6 +17,18 @@ describe('json', () => {
     key.set(rxjs.Observable.of({
       key: "key"
     })).toArray().subscribe((entries) => {
+      expect(entries).to.have.property('length', 1)
+      expect(entries).to.containSubset([{
+        $key: "key"
+      }])
+      done()
+    }, (error) => done(error))
+  })
+
+  it('Should use label', function (done) {
+    key.set(rxjs.Observable.of({
+      label: "key"
+    }),(entry)=>entry.label).toArray().subscribe((entries) => {
       expect(entries).to.have.property('length', 1)
       expect(entries).to.containSubset([{
         $key: "key"
