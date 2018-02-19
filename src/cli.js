2d148b2e579d6ee8f65a101a0a8dad0fbe94d1eb
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-19 21:12:00 +0100

    Executing load

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
+const main = require('./main')
+const rx = require('rxjs')
 const parameters = require('yargs')
   .command('$0 [baseDirectory] [targetFile]', 'load', (yargs) => {
     yargs.positional('baseDirectory',{default:'.'})
     .positional('targetFile',{default:'model.json'})
   })
   .argv
-console.log(parameters)
+main(parameters.baseDirectory,parameters.targetFile).subscribe(()=>console.log('done'))
