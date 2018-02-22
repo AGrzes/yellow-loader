9dac17a58178335e06cb952878a8b130dd8ede07
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-02-22 21:57:37 +0100

    Generating values

--- src/split.js
+++ src/split.js
@@ -8,6 +8,17 @@ function split(source,keyFunction,sink){
   return source.map((entity)=>{
     return _.mapValues(entity,(value,key)=>{
       if (isRelation(key)){
+        if (_.isString(value)){
+          value = {
+            $metadata:{
+              generated:true
+            },
+            $label:value
+          }
+        }
+        if (entity.$metadata){
+          value.$metadata = _.clone(entity.$metadata)
+        }
         sink(value)
         return keyFunction(value)
       } else {
--- test/split.js
+++ test/split.js
@@ -1,6 +1,7 @@
 const expect = require('chai').use(require('chai-subset')).expect
 const split = require('../src/split')
 const rx = require('rxjs')
+const _ = require('lodash')
 describe('split', () => {
   it('Should emit related', function (done) {
     const destination = new  rx.Subject()
@@ -21,5 +22,48 @@ describe('split', () => {
       destination.complete()
     }, (error) => done(error))
   })
- 
+  it('Should copy $metadata', function (done) {
+    const destination = new  rx.Subject()
+    destination.toArray().subscribe((entries) => {
+      expect(entries).to.have.property('length', 1)
+      expect(entries).to.containSubset([{
+        $metadata: "metadata",
+        "key":"value"
+      }])
+      done()
+    }, (error) => done(error))
+    split(rx.Observable.of({
+      $metadata: "metadata",
+      "@related": {"key":"value"}
+    }),(entity)=> entity.key,(entity)=>destination.next(entity)).toArray().subscribe((entries) => {
+      expect(entries).to.have.property('length', 1)
+      expect(entries).to.containSubset([{
+        $metadata: "metadata",
+        "@related":"value"
+      }])
+      destination.complete()
+    }, (error) => done(error))
+  })
+  it('Should generate related', function (done) {
+    const destination = new  rx.Subject()
+    destination.toArray().subscribe((entries) => {
+      expect(entries).to.have.property('length', 1)
+      expect(entries).to.containSubset([{
+        $metadata:{
+          generated: true
+        },
+        "$label":"Value"
+      }])
+      done()
+    }, (error) => done(error))
+    split(rx.Observable.of({
+      "@related": "Value"
+    }),(entity)=> _.kebabCase(entity.$label),(entity)=>destination.next(entity)).toArray().subscribe((entries) => {
+      expect(entries).to.have.property('length', 1)
+      expect(entries).to.containSubset([{
+        "@related":"value"
+      }])
+      destination.complete()
+    }, (error) => done(error))
+  })
 })
