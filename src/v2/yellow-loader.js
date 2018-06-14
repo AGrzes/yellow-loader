5f635b297a1ff620ef30af5de63dcf652d5f1e3a
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-06-14 22:08:45 +0200

    Calling scan

--- src/v2/yellow-loader.js
+++ src/v2/yellow-loader.js
@@ -3,9 +3,14 @@ const _ = require('lodash')
 class Loader {
   constructor(options){
     this.options = options
+    this.rules = []
   }
   add(rule){
     _.assign(rule,this.options)
+    this.rules.push(rule)
+  }
+  scan(){
+    _.forEach(this.rules,(rule)=>rule.scan())
   }
 }
 module.exports.Loader = Loader
--- test/v2/yellow-loader.js
+++ test/v2/yellow-loader.js
@@ -1,5 +1,6 @@
 const {Loader} = require('../../src/v2/yellow-loader')
-const expect = require('chai').expect
+const expect = require('chai').use(require('chai-sinon')).expect
+const sinon = require('sinon')
 describe('yellow-loader',function(){
   describe('loader',function(){
     it('Should pass options to rules',function(){
@@ -7,6 +8,14 @@ describe('yellow-loader',function(){
       const loader = new Loader({option:'value'})
       loader.add(rule)
       expect(rule).to.have.property('option','value')
+    })
+
+    it('Should call scan on all rules',function(){
+      const rule = { scan:sinon.spy()}
+      const loader = new Loader()
+      loader.add(rule)
+      loader.scan()
+      expect(rule.scan).to.have.been.called
     })
   })
   
