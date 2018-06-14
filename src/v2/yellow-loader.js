334130c9a0f9df5e0f3fe4d57403751ce9883872
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-06-14 21:56:43 +0200

    Assign options to rules

--- src/v2/yellow-loader.js
+++ src/v2/yellow-loader.js
@@ -1,49 +1,11 @@
-class LoadRule {
-  extract(extractFunction){
 
-  }
-}
-
-class Source {
-  file(glob){
-    return new LoadRule()
-  }
-  dir(glob){
-    return new LoadRule()
-  }
-}
-
-
+const _ = require('lodash')
 class Loader {
-  constructor(){
-    this.source = new Source(this)
+  constructor(options){
+    this.options = options
   }
-  use(plugin){
-
-  }
-  scan(target){
-
+  add(rule){
+    _.assign(rule,this.options)
   }
 }
-
-class Builder {
-  build(){
-    return new Loader()
-  }
-}
-
-module.exports.loader =  function loader(){
-  return new Loader()
-}
-
-module.exports.builder = () => new Builder()
-
-module.exports.parser = {
-  
-}
-
-module.exports.source = {
-  
-}
-
 module.exports.Loader = Loader
--- test/v2/yellow-loader.js
+++ test/v2/yellow-loader.js
@@ -1,9 +1,13 @@
-const yellowLoader = require('../../src/v2/yellow-loader')
+const {Loader} = require('../../src/v2/yellow-loader')
 const expect = require('chai').expect
 describe('yellow-loader',function(){
-  describe('builder',function(){
-    it('Should build loader',function(){
-      expect(yellowLoader.builder().build()).to.be.instanceof(yellowLoader.Loader)
+  describe('loader',function(){
+    it('Should pass options to rules',function(){
+      const rule = {}
+      const loader = new Loader({option:'value'})
+      loader.add(rule)
+      expect(rule).to.have.property('option','value')
     })
   })
+  
 })
