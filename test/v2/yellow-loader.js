7cfcc28392153717db1331e8120e3a1b38cbf706
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-06-14 22:09:29 +0200

    Calling scan on all rules

--- test/v2/yellow-loader.js
+++ test/v2/yellow-loader.js
@@ -11,11 +11,14 @@ describe('yellow-loader',function(){
     })
 
     it('Should call scan on all rules',function(){
-      const rule = { scan:sinon.spy()}
+      const rule1 = { scan:sinon.spy()}
+      const rule2 = { scan:sinon.spy()}
       const loader = new Loader()
-      loader.add(rule)
+      loader.add(rule1)
+      loader.add(rule2)
       loader.scan()
-      expect(rule.scan).to.have.been.called
+      expect(rule1.scan).to.have.been.called
+      expect(rule2.scan).to.have.been.called
     })
   })
   
